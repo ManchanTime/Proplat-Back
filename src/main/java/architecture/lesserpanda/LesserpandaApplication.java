aba0e8288d50e63ea33749390a6d11190f00e1ed
Lee <lych0918@gmail.com>
2023-09-26 19:10:16 +0900

    Initial commit

---
+++ src/main/java/architecture/lesserpanda/LesserpandaApplication.java
@@ -0,0 +1,13 @@
+package architecture.lesserpanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesserpandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesserpandaApplication.class, args);
+	}
+
+}
